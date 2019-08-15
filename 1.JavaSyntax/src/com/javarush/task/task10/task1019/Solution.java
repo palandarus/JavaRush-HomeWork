e76a30ae7a6471b94a63f141e4fd5e54b1df7ce6
palandarus <klarkken@yandex.ru>
2019-08-15 10:01:53 +0300

    new solved tasks

--- 1.JavaSyntax/src/com/javarush/task/task10/task1019/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1019/Solution.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task10.task1019;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /* 
 Функциональности маловато!
@@ -9,10 +11,23 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String id = "";
+        String name = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        while (true) {
+            try {
+                id = reader.readLine();
+                if (id.isEmpty()) break;
+                name = reader.readLine();
+                map.put(name, Integer.parseInt(id));
+            } catch (Exception e) {
+                //e.printStackTrace();
+            }
+        }
 
-        int id = Integer.parseInt(reader.readLine());
-        String name = reader.readLine();
+        for (Map.Entry<String, Integer> x : map.entrySet()) {
+            System.out.println(x.getValue() + " " + x.getKey());
+        }
 
-        System.out.println("Id=" + id + " Name=" + name);
     }
 }
