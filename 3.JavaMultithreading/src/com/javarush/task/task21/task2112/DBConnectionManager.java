58053402669e2b400e39f7ddb9e7c5dfb1a2cdf3
palandarus <klarkken@yandex.ru>
2019-08-16 15:46:00 +0300

    new solved tasks

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2112/DBConnectionManager.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task21.task2112;
+
+public class DBConnectionManager {
+    public FakeConnection getFakeConnection() {
+        return new FakeConnection();
+    }
+
+}
+
+
