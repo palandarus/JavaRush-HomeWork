4621d33225f742698bd5c0e247f802565458e2e6
palandarus <klarkken@yandex.ru>
2019-08-28 15:02:52 +0300

    new solved tasks

--- 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1721/Solution.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task17.task1721;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /* 
 Транзакционность
@@ -15,36 +14,28 @@ public class Solution {
     public static List<String> forRemoveLines = new ArrayList<String>();
 
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            String filename1 = reader.readLine();
-            String filename2 = reader.readLine();
-            FileInputStream file1 = new FileInputStream(filename1);
-            FileInputStream file2 = new FileInputStream(filename2);
-            reader.close();
-            BufferedReader readfile1 = new BufferedReader(new InputStreamReader(file1));
-            BufferedReader readfile2 = new BufferedReader(new InputStreamReader(file2));
-            String lines1;
-            while ((lines1 = readfile1.readLine()) != null) {
-                allLines.add(lines1);
-            }
-            String lines2;
-            while ((lines2 = readfile2.readLine()) != null) {
-                forRemoveLines.add(lines2);
-            }
+        try (BufferedReader fileInput1 = new BufferedReader(new FileReader(new BufferedReader(new InputStreamReader(System.in)).readLine()));
+             BufferedReader fileInput2 = new BufferedReader(new FileReader(new BufferedReader(new InputStreamReader(System.in)).readLine()))
+        ) {
+
+            allLines.addAll(fileInput1.lines().collect(Collectors.toList()));
+            forRemoveLines.addAll(fileInput2.lines().collect(Collectors.toList()));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        try {
+            Solution c = new Solution();
+            c.joinData();
+        } catch (CorruptedDataException e) {
+        }
     }
 
     public void joinData() throws CorruptedDataException {
-        for (int i = 0; i < allLines.size(); i++) {
-            for (String s : forRemoveLines) {
-                if (s.equals(allLines.get(i)))
-                    allLines.remove(s);
-            }
+        if (allLines.containsAll(forRemoveLines)) allLines.removeAll(forRemoveLines);
+        else{
+            allLines.clear();
+            throw new CorruptedDataException();
         }
     }
 }
-
